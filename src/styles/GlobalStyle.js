3c8de12fd524e4e38f6f2ec6b0d2dce8f4aaebad
jagn <jaegun0103@ajou.ac.kr>
2022-07-11 20:32:27 +0900

    style: global style 추가

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "segoe UI";
+  line-height: 1.7rem;
+  background-color: #000;
+  color: #fff;
+}
+`;
+
+export default GlobalStyle;
