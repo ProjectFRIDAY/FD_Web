111a07c4225a568f3dbc535a49191608a324b723
jagn <jaegun0103@ajou.ac.kr>
2022-07-11 19:56:14 +0900

    feat: ThemeProvider, GlobalStyle, Header, Route 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/GlobalStyle';
+import Main from './pages/Main';
+import Project from './pages/Project';
+import Recruit from './pages/Recruit';
+import Contact from './pages/Contact';
+import Header from './components/Header';
+
+const theme = {
+  primaryColor: '#01baae',
+  secondaryColor: '#000',
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Main />}></Route>
+            <Route exact path="/project" element={<Project />}></Route>
+            <Route exact path="/recruit" element={<Recruit />}></Route>
+            <Route exact path="/contact" element={<Contact />}></Route>
+            {/* <Route exact path="*" element={<NotFound />}></Route> */}
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
