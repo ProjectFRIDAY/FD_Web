7b879ba1c8deb13c4b693f40cf2a4895df0059c7
jagn <jaegun0103@ajou.ac.kr>
2022-07-12 01:03:14 +0900

    feat: button props refactoring (재사용성 향상)

--- src/components/main/Button.js
+++ src/components/main/Button.js
@@ -1,48 +1,67 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 import { lighten, darken } from 'polished';
 
-const PRIMARY_COLOR = props => props.theme.primaryColor;
-const COLOR = props => props.color;
-const BACK_COLOR = props => props.backgroundColor;
+Button.propTypes = {
+  color: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  gradient: PropTypes.bool,
+  border: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  color: '#01baae',
+  backgroundColor: 'rgba(0, 0, 0, 0)',
+  gradient: false,
+  border: false,
+};
 
 const StyledButton = styled.button`
-  /* 공통 스타일 */
-  display: inline-flex;
-  outline: none;
-  border: none;
-  border-radius: 10px;
-  color: ${COLOR || PRIMARY_COLOR}; // PRIMARY_COLOR 작동 안하는듯..
-  font-weight: bold;
-  cursor: pointer;
-  text-align: center;
-  padding-top: 3px;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  ${props =>
-    props.border &&
-    css`
-      border: 1px solid;
-      border-color: ${COLOR || PRIMARY_COLOR};
-    `}
-  /* 크기 */
-  height: 2.25rem;
-  font-size: 1rem;
-  /* 색상 */
-  background-color: ${BACK_COLOR || 'black'};
-  ${props =>
-    props.gradient &&
-    css`
-      background-image: linear-gradient(180deg, ${PRIMARY_COLOR}, ${darken(0.2, '#01baae')});
-    `}
-  &:hover {
-    color: ${lighten(0.1, '#01baae')};
-  }
-  &:active {
-    color: ${darken(0.1, '#01baae')};
-  }
+  ${props => {
+    const COLOR = props.color;
+    const BACKGROUND_COLOR = props.backgroundColor;
+    const BORDER = props.border;
+    const GRADIENT = props.gradient;
+    return css`
+      /* 공통 스타일 */
+      display: inline-flex;
+      outline: none;
+      border: none;
+      border-radius: 10px;
+      color: ${COLOR};
+      font-weight: bold;
+      cursor: pointer;
+      text-align: center;
+      padding-top: 3px;
+      padding-left: 1rem;
+      padding-right: 1rem;
+      ${BORDER &&
+      css`
+        border: 1px solid;
+        border-color: ${COLOR};
+      `}
+      /* 크기 */
+      height: 2.25rem;
+      font-size: 1rem;
+      /* 색상 */
+      background-color: ${BACKGROUND_COLOR};
+      ${GRADIENT &&
+      css`
+        background-image: linear-gradient(180deg, ${COLOR}, ${darken(0.2, COLOR)});
+      `}
+      &:hover {
+        color: ${lighten(0.1, COLOR)};
+      }
+      &:active {
+        color: ${darken(0.1, COLOR)};
+      }
+    `;
+  }}
 `;
 
-export default function Button({ children, ...rest }) {
+function Button({ children, ...rest }) {
   return <StyledButton {...rest}>{children}</StyledButton>;
 }
+
+export default Button;
