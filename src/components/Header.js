bab1a742158c94cebbbaaaa052649a9937bf8964
jagn <jaegun0103@ajou.ac.kr>
2022-07-13 14:34:35 +0900

    style: 미디어 쿼리 설정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 import { darken } from 'polished';
+import ModalMenu from './main/ModalMenu';
 
 const FixedHeader = styled.div`
   width: 100%;
@@ -34,39 +35,86 @@ const MyLink = styled(Link)`
       :hover {
         color: ${darken(0.2, 'white')};
       }
+      @media only screen and (max-width: 700px) {
+        display: none;
+      }
     `}
 `;
 
-function Header() {
-  // const [navigation, setNavigation] = useState(0);
-  // 각 url에 접속했을 때 헤더의 해당하는 네비게이션에 밑줄을 긋기 위한 state.
+const MenuIcon = styled.div`
+  maring: 0;
+  display: none;
+  cursor: pointer;
+  @media only screen and (max-width: 700px) {
+    position: absolute;
+    display: inline-block;
+  }
+`;
 
+const MenuBar = styled.div`
+  width: 35px;
+  height: 4px;
+  background-color: white;
+  margin: 6px 0;
+`;
+
+// const DropDownContainer = styled.div`
+//   float: left;
+//   overflow: hidden;
+//   @media only screen and (max-width: 700px) {
+//     position: absolute;
+//     display: inline-block;
+//   }
+// `;
+
+// const DropDownContent = styled.div`
+//   display: block;
+//   position: absolute;
+//   backgroud-color: 'white';
+//   min-width: 160px;
+//   z-index: 4;
+// `;
+
+function Header() {
+  const [menu, setMenu] = useState(1);
   return (
-    <FixedHeader>
-      <MyLink to="/">FRIDAY</MyLink>
-      <ul style={{ display: 'inline', position: 'absolute', right: '6rem' }}>
-        <VerticalLine>
-          <MyLink to="/" navigation>
-            HOME
-          </MyLink>
-        </VerticalLine>
-        <VerticalLine>
-          <MyLink to="/project" navigation>
-            PROJECT
-          </MyLink>
-        </VerticalLine>
-        <VerticalLine>
-          <MyLink to="/recruit" navigation>
-            RECRUIT
-          </MyLink>
-        </VerticalLine>
-        <VerticalLine>
-          <MyLink to="/contact" navigation>
-            CONTACT
-          </MyLink>
-        </VerticalLine>
-      </ul>
-    </FixedHeader>
+    <>
+      <FixedHeader>
+        <MyLink to="/">FRIDAY</MyLink>
+        <ul style={{ margin: 0, display: 'inline', position: 'absolute', right: '6rem' }}>
+          <VerticalLine>
+            <MyLink to="/" navigation>
+              HOME
+            </MyLink>
+          </VerticalLine>
+          <VerticalLine>
+            <MyLink to="/project" navigation>
+              PROJECT
+            </MyLink>
+          </VerticalLine>
+          <VerticalLine>
+            <MyLink to="/recruit" navigation>
+              RECRUIT
+            </MyLink>
+          </VerticalLine>
+          <VerticalLine>
+            <MyLink to="/contact" navigation>
+              CONTACT
+            </MyLink>
+          </VerticalLine>
+          <MenuIcon
+            onClick={() => {
+              menu ? setMenu(0) : setMenu(1);
+              console.log(menu);
+            }}
+          >
+            {menu ? <MenuBar /> : <div>x</div>}
+            <ModalMenu />
+          </MenuIcon>
+        </ul>
+      </FixedHeader>
+      {menu ? <ModalMenu /> : null}
+    </>
   );
 }
 
