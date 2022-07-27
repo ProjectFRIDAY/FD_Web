65471283e8db53f8852d9a451828f64f84e14829
jagn <jaegun0103@ajou.ac.kr>
2022-07-27 18:40:27 +0900

    feat: carousel 추가

--- src/components/main/ProjectIntro.js
+++ src/components/main/ProjectIntro.js
@@ -1,88 +1,140 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import Button from './Button';
-import comentoIcon from '../../assets/images/comento.png';
+import MyButton from './Button';
+import Icons from '../Icons';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-// let Slides = styled.img`
-// width: 100%;
-// height: 100%;
-// background: black;
-// border-radius: 20%;
-// `;
+const Container = styled.div`
+  height: 40rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 6rem auto;
+`;
 
-const FrontIcon = styled.img.attrs({
-  src: `${comentoIcon}`,
-})`
-  width: 10rem;
-  height: 10rem;
-  margin: 10 auto;
-  display: inline;
+const TextContainer = styled.div`
+  margin: 5rem auto;
+  font-size: 1.5vw;
+  line-height: 2rem;
+`;
+
+const MySlider = styled(Slider)`
+  width: 100%;
+`;
+
+const MyImg = styled.img`
+  width: 90%;
+  height: 90%;
+  outline: none;
   border-radius: 20px;
-  background-color: #000;
-  box-shadow: 0 3px 40px 0 #007b73;
-  border: 1.5px solid #01baae;
-  position: relative;
-`;
-
-const LeftIcon = styled.img.attrs({
-  src: `${comentoIcon}`,
-})`
-width: 7rem;
-height: 7rem;
-margin: 10 auto;
-display: inline;
-border-radius: 20px;
-box-shadow: 0 3px 40px 0 #007b73;
-border: 1.5px solid #01baae;
-position: relative;
-left: 3rem;
-filter: blur(2px);
-z-index = -1;
-`;
-
-const RightIcon = styled.img.attrs({
-  src: `${comentoIcon}`,
-})`
-width: 7rem;
-height: 7rem;
-margin: 10 auto;
-display: inline;
-border-radius: 20px;
-box-shadow: 0 3px 40px 0 #007b73;
-border: 1.5px solid #01baae;
-position: relative;
-right: 3rem;
-filter: blur(2px);
-z-index = -1;
+  border: 1.5px solid ${props => props.theme.primaryColor};
 `;
 
 function ProjectIntro() {
-  const [num, setNum] = useState();
-
-  return (
+  const icons = Icons.map(icon => {
     <div>
-      <div className="row ">
-        <div className="col-sm-4">
-          <LeftIcon></LeftIcon>
-          <FrontIcon></FrontIcon>
-          <RightIcon></RightIcon>
+      <MyImg key={icon.id} src={icon.src} />
+    </div>;
+  });
+  const settings = {
+    className: 'center',
+    centerMode: true,
+    centerPadding: '60px',
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 760,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <Container>
+      <TextContainer>
+        <div>
+          <h1>당신이 만들고 싶은 가치있는 서비스를 만듭니다.</h1>
+          {/* <p style={{ fontSize: '1.4vw' }}>
+            사용자들이 유용하게 사용할 수 있는 서비스를 개발 및 출시,<br></br>
+            운영하는 것을 목표로하는 조직입니다. 열정있는 기획자, 디자이너,<br></br>
+            개발자가 하나의 팀이 되어 새로운 가치를 창출합니다.
+          </p> */}
         </div>
-        <div className="col-sm-8">
-          <div>
-            <h1>comento</h1>
-            <h4>당신이 만들고 싶은 가치있는 서비스를 만듭니다.</h4>
-            <p>
-              사용자들이 유용하게 사용할 수 있는 서비스를 개발 및 출시,<br></br>
-              운영하는 것을 목표로하는 조직입니다. 열정있는 기획자, 디자이너,<br></br>
-              개발자가 하나의 팀이 되어 새로운 가치를 창출합니다.
-            </p>
-            <Button border color={'#01baae'} backgroundColor={'black'}>
-              모든 프로젝트
-            </Button>
-          </div>
+      </TextContainer>
+      <MySlider {...settings}>
+        <div>
+          <MyImg src={Icons[0].src} />
         </div>
-      </div>
-    </div>
+        <div>
+          <MyImg src={Icons[1].src} />
+        </div>
+        <div>
+          <MyImg src={Icons[2].src} />
+        </div>
+        <div>
+          <MyImg src={Icons[3].src} />
+        </div>
+        <div>
+          <MyImg src={Icons[4].src} />
+        </div>
+        <div>
+          <MyImg src={Icons[5].src} />
+        </div>
+        {/* {icons} */}
+      </MySlider>
+      <MyButton style={{ marginTop: '4rem' }} border color={'#01baae'} backgroundColor={'black'}>
+        모든 프로젝트 보기
+      </MyButton>
+    </Container>
+    // <Container>
+    //   <Positioner>
+    //     <CarouselContainer>
+    //       <Slider {...settings}>
+    //         <img src={Icons[0].src}></img>
+    //         <img src={Icons[1].src}></img>
+    //         <img src={Icons[2].src}></img>
+    //       </Slider>
+    //     </CarouselContainer>
+    //     <TextContainer>
+    //       <div>
+    //         <h1>comento</h1>
+    //         <h4>당신이 만들고 싶은 가치있는 서비스를 만듭니다.</h4>
+    //         <p>
+    //           사용자들이 유용하게 사용할 수 있는 서비스를 개발 및 출시,<br></br>
+    //           운영하는 것을 목표로하는 조직입니다. 열정있는 기획자, 디자이너,<br></br>
+    //           개발자가 하나의 팀이 되어 새로운 가치를 창출합니다.
+    //         </p>
+    //         <MyButton border color={'#01baae'} backgroundColor={'black'}>
+    //           모든 프로젝트 보기
+    //         </MyButton>
+    //       </div>
+    //     </TextContainer>
+    //   </Positioner>
+    // </Container>
   );
 }
 
