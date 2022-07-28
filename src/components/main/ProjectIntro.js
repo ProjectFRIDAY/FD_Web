fa93f3b4c3d821943069325bb1c290e1667efc34
jagn <jaegun0103@ajou.ac.kr>
2022-07-28 15:52:11 +0900

    style: media query 수정

--- src/components/main/ProjectIntro.js
+++ src/components/main/ProjectIntro.js
@@ -18,7 +18,9 @@ const Container = styled.div`
 const TextContainer = styled.div`
   margin: 5rem auto;
   font-size: 1.5vw;
-  line-height: 2rem;
+  @media only screen and (max-width: 600px) {
+    font-size: 2vw;
+  }
 `;
 
 const MySlider = styled(Slider)`
@@ -34,11 +36,11 @@ const MyImg = styled.img`
 `;
 
 function ProjectIntro() {
-  const icons = Icons.map(icon => {
-    <div>
-      <MyImg key={icon.id} src={icon.src} />
-    </div>;
-  });
+  // const icons = Icons.map(icon => {
+  //   <div>
+  //     <MyImg key={icon.id} src={icon.src} />
+  //   </div>;
+  // });
   const settings = {
     className: 'center',
     centerMode: true,
@@ -54,7 +56,7 @@ function ProjectIntro() {
     cssEase: 'linear',
     responsive: [
       {
-        breakpoint: 1200,
+        breakpoint: 1020,
         settings: {
           slidesToShow: 3,
         },
@@ -66,7 +68,7 @@ function ProjectIntro() {
         },
       },
       {
-        breakpoint: 550,
+        breakpoint: 443,
         settings: {
           slidesToShow: 1,
         },
@@ -78,7 +80,7 @@ function ProjectIntro() {
       <TextContainer>
         <div>
           <h1>당신이 만들고 싶은 가치있는 서비스를 만듭니다.</h1>
-          {/* <p style={{ fontSize: '1.4vw' }}>
+          {/* <p>
             사용자들이 유용하게 사용할 수 있는 서비스를 개발 및 출시,<br></br>
             운영하는 것을 목표로하는 조직입니다. 열정있는 기획자, 디자이너,<br></br>
             개발자가 하나의 팀이 되어 새로운 가치를 창출합니다.
@@ -110,31 +112,6 @@ function ProjectIntro() {
         모든 프로젝트 보기
       </MyButton>
     </Container>
-    // <Container>
-    //   <Positioner>
-    //     <CarouselContainer>
-    //       <Slider {...settings}>
-    //         <img src={Icons[0].src}></img>
-    //         <img src={Icons[1].src}></img>
-    //         <img src={Icons[2].src}></img>
-    //       </Slider>
-    //     </CarouselContainer>
-    //     <TextContainer>
-    //       <div>
-    //         <h1>comento</h1>
-    //         <h4>당신이 만들고 싶은 가치있는 서비스를 만듭니다.</h4>
-    //         <p>
-    //           사용자들이 유용하게 사용할 수 있는 서비스를 개발 및 출시,<br></br>
-    //           운영하는 것을 목표로하는 조직입니다. 열정있는 기획자, 디자이너,<br></br>
-    //           개발자가 하나의 팀이 되어 새로운 가치를 창출합니다.
-    //         </p>
-    //         <MyButton border color={'#01baae'} backgroundColor={'black'}>
-    //           모든 프로젝트 보기
-    //         </MyButton>
-    //       </div>
-    //     </TextContainer>
-    //   </Positioner>
-    // </Container>
   );
 }
 
