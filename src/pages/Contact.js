6bd3ba53c584e67b5921563bb00aa5e282f98027
jagn <jaegun0103@ajou.ac.kr>
2022-08-10 23:58:26 +0900

    feat: contact 페이지 추가

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const GradientBackground = styled.div`
+  width: 100%;
+  height: 1000px;
+  padding-left: 3rem;
+  padding-right: 3rem;
+  background-image: linear-gradient(148deg, #053a36 0%, #000 28%);
+`;
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 1rem;
+`;
+
+const Title = styled.h1``;
+
+const ContactLayout = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0rem;
+  padding: 0rem;
+`;
+
+const ContactBox = styled.div`
+  width: 20rem;
+  height: 5rem;
+  margin: 1rem;
+  background-color: #ccc;
+  color: black;
+  display: flex;
+  justify-content: space-around;
+  border-radius: 1rem;
+`;
 
 function Contact() {
-  return <div>Contact page.</div>;
+  return (
+    <div className="page">
+      <GradientBackground>
+        <Layout>
+          <h1 style={{ fontSize: '8vw' }}>Contact</h1>
+          <ContactLayout>
+            <ContactBox>메일</ContactBox>
+            <ContactBox>카카오톡</ContactBox>
+            <ContactBox>인스타그램</ContactBox>
+          </ContactLayout>
+        </Layout>
+      </GradientBackground>
+    </div>
+  );
 }
 
 export default Contact;
