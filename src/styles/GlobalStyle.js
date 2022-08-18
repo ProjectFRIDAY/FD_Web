7f97ca34fbbad02c04f4ad5dffff6535ee35e176
jagn <jaegun0103@ajou.ac.kr>
2022-08-18 16:51:19 +0900

    style: font 적용 및 재사용성 향상

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -4,16 +4,33 @@ const GlobalStyle = createGlobalStyle`
 *, *::before, *::after {
   box-sizing: border-box;
 }
+@font-face {
+  font-family: 'SegoeUI';
+  src: url('../assets/fonts/SegoeUI.TTF') format('woff2'),
+      url('../assets/fonts/SegoeUI.TTF') format('woff'),
+      url('../assets/fonts/SegoeUI.TTF') format('truetype');
+}
+@font-face {
+    font-family: 'TimesNewRomanPSMT';
+    src: url('../../assets/fonts/TimesNewRomanPSMT.ttf') format('woff2'), 
+    url('../../assets/fonts/TimesNewRomanPSMT.ttf') format('woff'), 
+    url('../../assets/fonts/TimesNewRomanPSMT.ttf') format('truetype');
+  }
+
 body {
   margin: 0;
   padding: 0;
-  font-family: "segoe UI";
+  font-family: 'SegoeUI';
   line-height: 1.7rem;
   background-color: #000;
   color: #fff;
 }
 .page {
   margin-top: 4rem;
+}
+.title {
+  font-family: 'TimesNewRomanPSMT';
+  font-weight: normal;
 }
 `;
 
