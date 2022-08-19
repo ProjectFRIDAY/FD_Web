ced533afeb7e5f7443f3555bcfc19353a681015a
jagn <jaegun0103@ajou.ac.kr>
2022-08-19 13:51:12 +0900

    style: 폰트 추가

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,26 +1,28 @@
 import { createGlobalStyle } from 'styled-components';
+import SegoeUI from '../assets/fonts/SegoeUI.TTF';
+import Times from '../assets/fonts/TimesNewRomanPSMT.ttf';
 
 const GlobalStyle = createGlobalStyle`
 *, *::before, *::after {
   box-sizing: border-box;
-}
+} 
 @font-face {
-  font-family: 'SegoeUI';
-  src: url('../assets/fonts/SegoeUI.TTF') format('woff2'),
-      url('../assets/fonts/SegoeUI.TTF') format('woff'),
-      url('../assets/fonts/SegoeUI.TTF') format('truetype');
+  font-family: 'SegoeUI', sans-serif;
+  src: url(${SegoeUI}) format('woff2'),
+      url(${SegoeUI}) format('woff'),
+      url(${SegoeUI}) format('truetype');
 }
 @font-face {
     font-family: 'TimesNewRomanPSMT';
-    src: url('../../assets/fonts/TimesNewRomanPSMT.ttf') format('woff2'), 
-    url('../../assets/fonts/TimesNewRomanPSMT.ttf') format('woff'), 
-    url('../../assets/fonts/TimesNewRomanPSMT.ttf') format('truetype');
+    src: url(${Times}) format('woff2'), 
+    url(${Times}) format('woff'), 
+    url(${Times}) format('truetype');
   }
 
 body {
   margin: 0;
   padding: 0;
-  font-family: 'SegoeUI';
+  font-family: 'SegoeUI', sans-serif;
   line-height: 1.7rem;
   background-color: #000;
   color: #fff;
