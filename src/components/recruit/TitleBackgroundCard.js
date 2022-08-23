2d149c8c50a8c5f6a87dfbdf5ff9a265288f9da0
jagn <jaegun0103@ajou.ac.kr>
2022-08-23 16:02:08 +0900

    style: 버튼 스타일 변경

--- src/components/recruit/TitleBackgroundCard.js
+++ src/components/recruit/TitleBackgroundCard.js
@@ -1,30 +1,45 @@
 import React from 'react';
 import BackgroundImage from '../../assets/images/recruitBackground.png';
-import Button from '../../components/main/Button'
+import Button from '../../components/main/Button';
 
 function TitleBackgroundCard() {
-    return (
-    <div style={{ 
-      backgroundImage: `url(${BackgroundImage})`, 
-      backgroundSize: 'cover',
-      height: '600px',
-      margin: '0',
-      display: 'flex',
-      flexDirection: 'column',
-      }}>
-        <p style={{
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${BackgroundImage})`,
+        backgroundSize: 'cover',
+        height: '600px',
+        margin: '0',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <p
+        style={{
           fontFamily: 'TimesNewRomanPSMT',
           fontSize: '80px',
           paddingTop: '250px',
-          marginLeft: '20vw'
-        }}>FRIDAY 모집</p>
-        <Button style={{
+          marginLeft: '20vw',
+        }}
+      >
+        FRIDAY 모집
+      </p>
+      <Button
+        style={{
           width: '160px',
           height: '50px',
-          marginLeft: '20vw'
-        }}>
-          10기 지원하기 →
-        </Button>
-    </div>);
+          marginLeft: '20vw',
+        }}
+        color={'black'}
+        gradient
+        border
+        backgroundColor={'white'}
+        href="https://wjsalsrb5.typeform.com/friday-apply"
+        target="_blank"
+      >
+        2기 지원하기
+      </Button>
+    </div>
+  );
 }
-export default TitleBackgroundCard
+export default TitleBackgroundCard;
