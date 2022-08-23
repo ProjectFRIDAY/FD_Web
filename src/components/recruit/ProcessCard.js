bc3875f6db4e72ecc80e88dfae13b8aff6c26894
jagn <jaegun0103@ajou.ac.kr>
2022-08-23 16:01:00 +0900

    style: 폰트 사이즈 변경

--- src/components/recruit/ProcessCard.js
+++ src/components/recruit/ProcessCard.js
@@ -1,61 +1,63 @@
 import React from 'react';
 import styled from 'styled-components';
-import * as Text from './Text'
+import * as Text from './Text';
 
 const ProcessBigText = styled(Text.Big)`
   width: 30vw;
-`
+`;
 const ProcessMidTextContainer = styled.div`
   left: 50vw;
   display: flex;
   flex-direction: column;
-`
+`;
 const ProcessMidText = styled(Text.Mid)`
   margin-bottom: 20px;
-`
+`;
 const ProcessTinyText = styled(Text.Tiny)`
   margin-bottom: 50px;
-`
+  font-size: 13px;
+  line-height: 35px;
+`;
 function ProcessCard() {
   return (
-    <div style={{
-      backgroundImage: 'linear-gradient(148deg, #053a36 0%, #000 60%)',
-      paddingTop: '100px',
-      display: 'flex',
-      flexDirection: 'row',
-      alignItmes: 'flex-start',
-      justifyContent: 'flex-around',
-      paddingLeft: '20vw',
-      flexWrap: 'wrap'
-    }}>
+    <div
+      style={{
+        backgroundImage: 'linear-gradient(148deg, #053a36 0%, #000 60%)',
+        paddingTop: '100px',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItmes: 'flex-start',
+        justifyContent: 'flex-around',
+        paddingLeft: '20vw',
+        flexWrap: 'wrap',
+      }}
+    >
       <ProcessBigText>PROCESS</ProcessBigText>
       <ProcessMidTextContainer>
         <ProcessMidText>모집기간</ProcessMidText>
-        <ProcessTinyText>상시 모집</ProcessTinyText>
+        <ProcessTinyText>매년 3월, 9월</ProcessTinyText>
         <ProcessMidText>지원자격</ProcessMidText>
         <ProcessTinyText>
-          6개월 동안 활동하는데 문제가 없고, 력을 키우기 위해 배우려는<br/>
-          의지가 있는 사람<br/>
-          (대학생이 아닌 졸업생, 일반인도 가능)
+          1. Friday와 동일한 목표를 가지신 분
+          <br />
+          2. Friday에 많은 시간을 투자할 수 있는 분
+          <br />
+          3. 지원 분야에 대한 간단한 지식이 있는 분
         </ProcessTinyText>
         <ProcessMidText>접수방법</ProcessMidText>
-        <ProcessTinyText>
-          6개월 동안 활동하는데 문제가 없고, 력을 키우기 위해 배우려는<br/>
-          의지가 있는 사람<br/>
-        </ProcessTinyText>
+        <ProcessTinyText>상단의 지원하기 버튼을 통해 지원서를 작성해주세요.</ProcessTinyText>
         <ProcessMidText>전형절차</ProcessMidText>
+        <ProcessTinyText> 지원자님은 간단한 지원서를 작성하신 후, 짧은 비대면 면접을 거치게 됩니다.</ProcessTinyText>
         <Text.SmallBold>서류전형</Text.SmallBold>
-        <Text.Tiny>
-          제출하신 지원서를 바탕으로 모집<br/>
-          직군과의 연관성을 종합적으로 검토합니다.
-        </Text.Tiny>
+        <ProcessTinyText>제출하신 지원서를 바탕으로 지원 분야와의 연관성을 종합적으로 검토합니다.</ProcessTinyText>
         <Text.SmallBold>1차 면접</Text.SmallBold>
-        <Text.Tiny>
-          실무에 대한 내용을 중심으로 면접이 진행됩니다.<br/>
+        <ProcessTinyText>
+          실무에 대한 내용을 중심으로 면접이 진행됩니다.
+          <br />
           면접 시간은 약 30분 ~ 1시간 이내로 소요됩니다.
-        </Text.Tiny>
+        </ProcessTinyText>
       </ProcessMidTextContainer>
     </div>
-  )
+  );
 }
 export default ProcessCard;
