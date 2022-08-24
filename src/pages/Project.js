4619f0305ad7e0f2d8151499cf076f6d1609ac6f
vSsongv <songdh21@naver.com>
2022-08-25 06:15:12 +0900

    ✨ feat: projects import

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -7,6 +7,7 @@ import Icons from '../components/Icons';
 import card from '../components/project/Card';
 import Button from '../components/Button';
 import Modal from '../components/project/Modal';
+import Projects from '../components/project/Projects';
 
 const StyledH1 = styled.h1`
   width: 100%;
@@ -73,8 +74,8 @@ const MainBottom = styled.div`
 function Project() {
   const [visible, setVisible] = useRecoilState(visibleState);
   const [prj, setPrj] = useRecoilState(prjState);
-  const openModal = num => {
-    setPrj(num);
+  const openModal = prj => {
+    setPrj(prj);
     setVisible(true);
   };
 
@@ -86,7 +87,7 @@ function Project() {
       <Wrapper>
         <card.Card
           onClick={() => {
-            openModal(1);
+            openModal('플랜다이얼');
           }}
         >
           <card.ImageField src={Icons[1].src}></card.ImageField>
@@ -98,10 +99,10 @@ function Project() {
         </card.Card>
         <card.Card
           onClick={() => {
-            openModal(2);
+            openModal('웹사이트');
           }}
         >
-          <card.ImageField src={Icons[6].src}></card.ImageField>
+          <card.ImageField src={Icons[2].src}></card.ImageField>
           <card.TextField>Friday 공식 웹사이트</card.TextField>
           <card.TagField>
             <card.Tag>#개발중</card.Tag>
@@ -110,7 +111,7 @@ function Project() {
         </card.Card>
         <card.Card
           onClick={() => {
-            openModal(3);
+            openModal('comento');
           }}
         >
           <card.ImageField src={Icons[0].src}></card.ImageField>
@@ -124,7 +125,7 @@ function Project() {
       <Wrapper>
         <card.Card
           onClick={() => {
-            openModal(4);
+            openModal('엄마타이머');
           }}
         >
           <card.ImageField src={Icons[3].src}></card.ImageField>
@@ -136,31 +137,29 @@ function Project() {
         </card.Card>
         <card.Card
           onClick={() => {
-            openModal(5);
+            openModal('약속이');
           }}
         >
           <card.ImageField src={Icons[4].src}></card.ImageField>
           <card.TextField>약속이</card.TextField>
           <card.TagField>
-            <card.Tag>#개발완료</card.Tag>
-            <card.Tag>#WEB</card.Tag>
             <card.Tag>#토이프로젝트</card.Tag>
+            <card.Tag>#WEB</card.Tag>
           </card.TagField>
         </card.Card>
         <card.Card
           onClick={() => {
-            openModal(5);
+            openModal('텍스티');
           }}
         >
           <card.ImageField src={Icons[5].src}></card.ImageField>
           <card.TextField>텍스티</card.TextField>
           <card.TagField>
-            <card.Tag>#개발완료</card.Tag>
-            <card.Tag>#WEB</card.Tag>
             <card.Tag>#토이프로젝트</card.Tag>
+            <card.Tag>#WEB</card.Tag>
           </card.TagField>
         </card.Card>
-        <Modal content={prj} visible={visible}></Modal>
+        <Modal content={Projects[prj]} visible={visible}></Modal>
       </Wrapper>
       <MainBottom>
         <h1>Open Up Your Ideas</h1>
