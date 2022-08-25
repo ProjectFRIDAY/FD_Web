bf0cb26dddbdc3b59f91580e3e0091e977c37f7c
jagn <jaegun0103@ajou.ac.kr>
2022-08-26 01:06:17 +0900

    style: 폰트 format 추가

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,6 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
-import SegoeUI from '../assets/fonts/SegoeUI.TTF';
-import Times from '../assets/fonts/TimesNewRomanPSMT.ttf';
+import SegoeTTF from '../assets/fonts/SegoeUI.TTF';
+import SegoeEot from '../assets/fonts/SegoeUI.eot';
+import SegoeWoff from '../assets/fonts/SegoeUI.woff';
+import SegoeWoff2 from '../assets/fonts/SegoeUI.woff2';
+import TimesTTF from '../assets/fonts/TimesNewRomanMTStd.ttf';
+import TimesEot from '../assets/fonts/TimesNewRomanMTStd.eot';
+import TimesWoff from '../assets/fonts/TimesNewRomanMTStd.woff';
+import TimesWoff2 from '../assets/fonts/TimesNewRomanMTStd.woff2';
 
 const GlobalStyle = createGlobalStyle`
 *, *::before, *::after {
@@ -8,15 +14,17 @@ const GlobalStyle = createGlobalStyle`
 } 
 @font-face {
   font-family: 'SegoeUI', sans-serif;
-  src: url(${SegoeUI}) format('woff2'),
-      url(${SegoeUI}) format('woff'),
-      url(${SegoeUI}) format('truetype');
+  src: url(${SegoeWoff2}) format('woff2'),
+      url(${SegoeWoff}) format('woff'),
+      url(${SegoeTTF}) format('truetype'),
+      url(${SegoeEot}+'?iefix') format('embedded-opentype');
 }
 @font-face {
     font-family: 'TimesNewRomanPSMT';
-    src: url(${Times}) format('woff2'), 
-    url(${Times}) format('woff'), 
-    url(${Times}) format('truetype');
+    src: url(${TimesWoff2}) format('woff2'), 
+    url(${TimesWoff}) format('woff'), 
+    url(${TimesTTF}) format('truetype'),
+    url(${TimesEot}+'?iefix') format('embedded-opentype');
   }
 
 body {
@@ -25,19 +33,8 @@ body {
   font-family: 'SegoeUI', sans-serif;
   line-height: 1.7rem;
   background-color: #000;
+  text-decoration: none;
   color: #fff;
-}
-
-a:link { 
- text-decoration: none;
-}
- 
-a:visited { 
- text-decoration: none;
-}
- 
-a:hover { 
- text-decoration: underline;
 }
 
 .page {
