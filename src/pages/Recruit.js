eca72b9fbecd034b14c8a3b4d67e788a9a0ae1a0
jagn <jaegun0103@ajou.ac.kr>
2022-08-25 22:45:45 +0900

    feat: 페이지 랜더링 시 메뉴 모달 제거

--- src/pages/Recruit.js
+++ src/pages/Recruit.js
@@ -3,10 +3,14 @@ import TitleBackgroundCard from '../components/recruit/TitleBackgroundCard';
 import ProcessCard from '../components/recruit/ProcessCard';
 import RecruitAreaCard from '../components/recruit/RecruitAreaCard';
 import FAQCard from '../components/recruit/FAQCard';
+import { menuAtom } from '../recoil/atom';
+import { useSetRecoilState } from 'recoil';
 
 function Recruit() {
+  const setMenu = useSetRecoilState(menuAtom);
   return (
     <>
+      {setMenu(1)}
       <TitleBackgroundCard />
       <ProcessCard />
       <RecruitAreaCard />
