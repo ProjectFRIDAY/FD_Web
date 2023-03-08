244be2d3a2b1905166e2a34e8fcc385170cc4a6e
Mango-Juice <wjsalsrb5@naver.com>
2023-03-08 11:52:04 +0900

    fix: 오타 수정

--- pages/post.js
+++ pages/post.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { useSetRecoilState } from 'recoil';
 import { useRouter } from 'next/router';
-import { getNotionPost } from '../src/data/source/notion';
+import { getNotionPost } from '../src/data/source/NotionApi';
 import AnimatedPage from '../src/components/template/AnimatedPage';
 import ScrollToTopButton from '../src/components/ScrollToTopButton';
 import { menuAtom } from '../src/recoil/atom';
