e5e8a372e5700cb650e817f74d095a5740cbbf4e
Mango-Juice <wjsalsrb5@naver.com>
2023-04-02 22:45:17 +0900

    feat: 신규 모집 계획 없음 문구 추가

--- src/components/JoinFriday.js
+++ src/components/JoinFriday.js
@@ -25,7 +25,7 @@ const JoinFriday = () => {
       <JoinBottom>
         <h1>Open Up Your Ideas</h1>
         <Button target="_blank" backgroundColor={'#01baae'} color={'white'} gradient>
-          <MyLink to="/recruit" fontSize="1rem">5기 지원하기</MyLink>
+          <MyLink to="/recruit" fontSize="1rem">모집 공고 확인하기</MyLink>
         </Button>
       </JoinBottom>
     </ScrollAnimation>
