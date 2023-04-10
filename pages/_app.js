7935ddf7823609e686f2fda6e5cf96533930ce10
Mango-Juice <wjsalsrb5@naver.com>
2023-04-10 17:41:46 +0900

    fix: 메타 태그 미적용 이슈 해결

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import GlobalStyle from '../src/styles/GlobalStyle';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
@@ -11,6 +11,13 @@ const theme = {
 };
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    console.log(
+      '%c⭐️ FRIDAY ⭐️',
+      'color: #01B9AD; background: #000; font-size: 3rem; padding: 0.5rem 1rem; margin: 1rem 0; font-family: TimesNewRomanPSMT; border: 2px solid #01B9AD; border-radius: 0.5rem; text-shadow: 1px 1px 1px #C3EAE6;'
+    );
+    console.log('Copyright © 2020-2023 Friday. All rights reserved.');
+  }, []);
   return (
     <React.StrictMode>
       <div className="App">
